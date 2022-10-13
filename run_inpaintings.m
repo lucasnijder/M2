958cba6a339a3e6bca61f7e2670662b3e80a474c
lucasnijder <l.l.snijder@student.tue.nl>
2022-10-13 15:26:26 +0200

    resulting images are now saved in results folder

--- run_inpaintings.m
+++ run_inpaintings.m
@@ -1,10 +1,11 @@
 clearvars;
 
 % Change name to image you want to inpaint
-name= './images/tests/image2';
+img_name = 'image2';
+path_name= append('./images/tests/',img_name);
 
 % set I to the original image (with holes)
-I = double(imread([ name '_toRestore.jpg']));
+I = double(imread([ path_name '_toRestore.jpg']));
 
 %Number of pixels for each dimension, and number of channles
 [ni, nj, nC] = size(I);
@@ -18,7 +19,7 @@ I=I-min(I(:));
 I=I/max(I(:));
 
 %Load the mask
-mask_img = double(imread([name '_mask.jpg']));
+mask_img = double(imread([path_name '_mask.jpg']));
 %mask_img =mask_img(1:10,1:10);
 [ni, nj, nC] = size(mask_img);
 if nC==3
@@ -38,23 +39,31 @@ param.tol = 10^-5;
 param.hi = 1 / (ni-1);
 param.hj = 1 / (nj-1);
 
-figure(1)
+img_before = figure(1);
 imshow(I);
 title('Before')
 
+image_before_save_name = append('./results/tests/', img_name,'_before.png');
+saveas(img_before,image_before_save_name);
+
 Iinp=team_MLR(I, mask, param);
-figure(2)
+    
+img_after = figure(2);
 imshow(Iinp)
 title('After');
+
+image_after_save_name = append('./results/tests/', img_name,'_after.png');
+saveas(img_after, image_after_save_name);
 
 %% Challenge image. (We have lost 99% of information)
 clearvars;
 
 % Change name to image you want to inpaint
-name= './images/tests/image5';
+img_name = 'image5';
+path_name= append('./images/tests/',img_name);
 
 % set I to the original image (with holes)
-I = double(imread([ name '_toRestore.jpg']));
+I = double(imread([ path_name '_toRestore.jpg']));
 
 %Normalize values into [0,1]
 I=I/256;
@@ -63,7 +72,7 @@ I=I/256;
 %Number of pixels for each dimension, and number of channels
 [ni, nj, nC] = size(I);
 
-mask_img = double(imread([name '_mask.jpg']));
+mask_img = double(imread([path_name '_mask.jpg']));
 mask = mask_img >128; %mask(i,j) == 1 means we have lost information in that pixel
                       %mask(i,j) == 0 means we have information in that
                       %pixel
@@ -71,25 +80,34 @@ mask = mask_img >128; %mask(i,j) == 1 means we have lost information in that pix
 param.hi = 1 / (ni-1);
 param.hj = 1 / (nj-1);
 
-figure(1)
+img_before = figure(1);
 imshow(I);
 title('Before')
+
+image_before_save_name = append('./results/tests/', img_name,'_before.png');
+saveas(img_before,image_before_save_name);
 
 Iinp(:,:,1)=team_MLR(I(:,:,1), mask(:,:,1), param);
 Iinp(:,:,2)=team_MLR(I(:,:,2), mask(:,:,2), param);
 Iinp(:,:,3)=team_MLR(I(:,:,3), mask(:,:,3), param);
-
-figure(2)
+    
+img_after = figure(2);
 imshow(Iinp)
 title('After');
+
+image_after_save_name = append('./results/tests/', img_name,'_after.png');
+saveas(img_after, image_after_save_name);
+
+
 
 %% Goal Image
 clearvars;
 
 %Read the image
-name= './images/tests/image';
+img_name = 'image';
+path_name= append('./images/tests/',img_name);
 
-I = double(imread([ name '_toRestore.png']));
+I = double(imread([ path_name '_toRestore.png']));
 
 [ni, nj, nC] = size(I);
 
@@ -116,23 +134,33 @@ param.hi = 1 / (ni-1);
 param.hj = 1 / (nj-1);
 
 % for each channel 
-figure(1)
+img_before = figure(1);
 imshow(I);
 title('Before')
+
+image_before_save_name = append('./results/tests/', img_name,'_before.png');
+saveas(img_before,image_before_save_name);
 
 Iinp(:,:,1)=team_MLR(I_ch1, mask, param);
 Iinp(:,:,2)=team_MLR(I_ch2, mask, param);
 Iinp(:,:,3)=team_MLR(I_ch3, mask, param);
     
-figure(2)
+img_after = figure(2);
 imshow(Iinp)
 title('After');
+
+image_after_save_name = append('./results/tests/', img_name,'_after.png');
+saveas(img_after, image_after_save_name);
 
 %% Example images
 clearvars;
 
+% select the image, choose from: night, social, desert
+img_name = 'night';
+path_name= append('./images/examples/',img_name);
+
 %Read the image
-I = double(imread('./images/examples/desert_toRestore.png'));
+I = double(imread([ path_name '_toRestore.png']));
 
 [ni, nj, nC] = size(I);
 
@@ -145,7 +173,7 @@ I_ch1 = I(:,:,1);
 I_ch2 = I(:,:,2);
 I_ch3 = I(:,:,3);
 
-mask_img = double(imread('./images/examples/desert_mask.png'));
+mask_img = double(imread([path_name '_mask.png']));
 mask = mask_img > 128;
 %TO COMPLETE 1
  %mask_img(i,j) == 1 means we have lost information in that pixel
@@ -166,7 +194,8 @@ img_before = figure(1);
 imshow(I);
 title('Before')
 
-saveas(img_before,'./images/examples/desert_before.png');
+image_before_save_name = append('./results/examples/', img_name,'_before.png');
+saveas(img_before,image_before_save_name);
 
 Iinp(:,:,1)=team_MLR(I_ch1, mask, param);
 Iinp(:,:,2)=team_MLR(I_ch2, mask, param);
@@ -176,6 +205,7 @@ img_after = figure(2);
 imshow(Iinp)
 title('After');
 
-saveas(img_after,'./images/examples/desert_after.png');
+image_after_save_name = append('./results/examples/', img_name,'_after.png');
+saveas(img_after,image_after_save_name);
 
 return;
